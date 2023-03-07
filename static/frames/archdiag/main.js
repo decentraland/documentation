8b2e988e6f94b8733c43cc5201057597a72668c1
Santiago Lezica <salezica@gmail.com>
2023-03-07 15:31:23 -0300

    architecture diagram: add iframe sources

---
+++ static/frames/archdiag/main.js
@@ -0,0 +1,35 @@
+
+document.querySelectorAll(".box > .cover").forEach(el => {
+  const expanded = { minWidth: '100%', minHeight: '100%' }
+  const contracted = { minWidth: '0', minHeight: '0' }
+
+  const durationStr = getComputedStyle(document.documentElement)
+    .getPropertyValue('--animation-short')
+    .slice(0, -1)
+
+  const options = {
+    duration: parseFloat(durationStr) * 1000,
+    fill: 'both',
+    easing: 'ease-out'
+  }
+
+  el.onclick = () => {
+    let animation
+
+    if (el.classList.contains('expanded')) {
+      animation = el.animate([ expanded, contracted ], options)
+      el.classList.remove('expanded')
+
+    } else {
+      animation = el.animate([ contracted, expanded ], options)
+      el.classList.add('expanded')
+    }
+
+    el.classList.add('animating')
+    animation.finished.then(() => el.classList.remove('animating'))
+  }
+})
+
+function onBoxClick(id) {
+  window.parent.postMessage(`${window.frameElement.id}:click:${id}`)
+}
